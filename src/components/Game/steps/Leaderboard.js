c297116407eff3fb0dc0b58c18557b3107e83ba6
helly <8hellyp@gmail.com>
2020-02-14 20:06:20 -0500

    veg-TABLE

--- src/components/Game/steps/Leaderboard.js
+++ src/components/Game/steps/Leaderboard.js
@@ -1,17 +1,22 @@
-//-------------------------------------------- OMAR'S CODE
-import React , {useEffect, useContext}from 'react'
+import React , {useEffect}from 'react'
 import Play from './steps/Play';
 import axios from 'axios';
 import './steps/Answers.css'
-//import {UserContext} from '../Context/UserContext';
-import {QuizContext} from '../Context/QuizContext';
 
 export default function GameLayout(props) {
-    const { quiz } = useContext(QuizContext);
 
     useEffect(() => {
-        console.log(quiz);
-    },[quiz]);
+       async function fetch(){
+           try {
+            const resp = await axios('http://localhost:5000/api/v1/users');
+            console.log(resp.data)
+           } catch (error) {
+               console.log(error)
+           }
+            
+        }
+        fetch();
+    });
 
     const arr = [   {username:"Omar5000", points:"732"},
     {username:"HarelP",     points:"755"},
@@ -19,9 +24,8 @@ export default function GameLayout(props) {
     {username:"BigBob",      points:"795"},
     {username:"ILoveTRUMP",      points:"795"},
     {username:"ILoveTRUMP",      points:"795"}]
-    //console.log(quiz);
 
-    const notNull = quiz !== null;
+
     return (
         <div className="row" style={{ height: "85vh" }}>
             <div className="col l2 s12 hideMobile">
@@ -31,18 +35,11 @@ export default function GameLayout(props) {
                             <th>Username</th>
                             <th>Points</th>
                         </tr>
-                                {/* {quiz.map((el, index)=>{
+
+                              {arr.map((el, index)=>{
                                 return (
                                     <tr key={index} >
-                                            <th>{el.dateCreated}</th>
-                                           
-                                    </tr>
-                                )
-                            })} */}
-                              {notNull && quiz.players.map((el, index)=>{
-                                return (
-                                    <tr key={index} >
-                                            <th>{el.playerName}</th>
+                                            <th>{el.username}</th>
                                             <th>{el.points}</th>
                                     </tr>
                                 )
@@ -58,6 +55,3 @@ export default function GameLayout(props) {
 }
 
 // setup quiz specific leaderboard for every quiz
-
-
-
